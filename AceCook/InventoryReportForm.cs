787ce9474c4966b5d3a6a46321325ec9fb96066a
Truong Tri Hao <truongtrihao13@gmail.com>
2025-08-15 22:28:24 +0700

    bbbbbb

--- AceCook/InventoryReportForm.cs
+++ AceCook/InventoryReportForm.cs
@@ -281,7 +281,7 @@ namespace AceCook
             var card = new Panel
             {
                 Width = 350, // Increased from 280 to 350 to accommodate longer Vietnamese text
-                Height = 40,
+                Height = 120,
                 BackColor = Color.White,
                 BorderStyle = BorderStyle.FixedSingle,
                 // Margin = new Padding(20, 0, 20, 0)
